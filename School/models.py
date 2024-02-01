3b586b6d41771458cad77c7d395c5dca92055d2a
bhawani0x <bsrathore1122571@gmail.com>
2024-02-01 12:28:49 +0530

    Update the models

--- School/models.py
+++ School/models.py
@@ -4,6 +4,8 @@ from django.db import models
 # Create your models here.
 class Class(models.Model):
     name = models.CharField(max_length=10, null=False, blank=False)
+    Class_teacher_name = models.CharField(max_length=10, null=True, blank=True)
+    class_teacher_number = models.IntegerField(null=True, blank=True, max_length=10)
 
     def __str__(self):
         return self.name
@@ -11,8 +13,11 @@ class Class(models.Model):
 
 class Student(models.Model):
     Class = models.ForeignKey(Class, on_delete=models.PROTECT)
-    name = models.CharField(max_length=15, null=False, blank=False)
+    first_name = models.CharField(max_length=15, null=False, blank=False)
+    last_name = models.CharField(max_length=15, null=False, blank=False)
     dob = models.DateField(auto_now_add=False)
+    Father_name = models.CharField(max_length=20, null=True, blank=True)
+    address = models.TextField(null=True, blank=True)
 
     def __str__(self):
         return self.name
